6739f71821a85a7abcdc236eb33f05197a48d1db
Neeraj <nxxwxj@gmail.com>
2022-01-31 23:26:20 +0530

    completed Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdOutlineRestaurantMenu } from 'react-icons/md'
 
-import './Navbar.css';
+import images from '../../constants/images'
+import './Navbar.css'
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
 
-export default Navbar;
+  const linkItems = (
+    <Fragment>
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#about">About</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#menu">Menu</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#awards">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#contact">Contact</a>
+      </li>
+    </Fragment>
+  )
+
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="site logo" />
+      </div>
+      <ul className="app__navbar-links">{linkItems}</ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Login / Register
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book a Table
+        </a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="app__navbar-smallscreen_links">{linkItems}</ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
